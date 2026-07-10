5e69994e439c25e0644947c649c9d319538cc74f
08abhinav <abhinavnegi101@gmail.com>
2026-07-10 11:53:17 +0530

    spring implementation for spring injection

---
+++ src/main/java/com/Abhinav/SpringDemo/App.java
@@ -0,0 +1,15 @@
+package com.Abhinav.SpringDemo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+       
+        //Dependency Injection
+        //1. Setter injection
+        Car car = (Car) ctx.getBean("car");
+        System.out.println("Car battery remainig: " + car.getBattery()+ "%");
+    }
+}
---
+++ src/main/java/com/Abhinav/SpringDemo/Car.java
@@ -0,0 +1,13 @@
+package com.Abhinav.SpringDemo;
+
+public class Car {
+	private int battery;
+
+	public int getBattery() {
+		return battery;
+	}
+
+	public void setBattery(int battery) {
+		this.battery = battery;
+	}
+}
